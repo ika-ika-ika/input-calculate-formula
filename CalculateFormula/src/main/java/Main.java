85fc6f73abf106d28fa71437e4f60f98459ccff3
ika-ika-ika <iskander.makhmudov@gmail.com>
2022-05-31 16:32:10 -0500

    learning with Yury 2

--- CalculateFormula/src/main/java/Main.java
+++ CalculateFormula/src/main/java/Main.java
@@ -10,29 +10,37 @@ public class Main {
         System.out.println("First, input X, Y and Z:");
         // Ввод трех переменных - X, Y & Z
         double varX = input.nextDouble();
+
+        // Проверка логарифма на положительное значение
+
+        while (Math.log10(2 + Math.exp(varX)) < 0) {
+            System.out.println("That's an error, please other value of X");
+            varX = input.nextDouble();
+        }
+
         double varY = input.nextDouble();
         double varZ = input.nextDouble();
+        // Определяем переменную для одинакового подвыражения
+        double varSameDivision = Math.cos(2 * varZ - 1);
 
         // Проверка на область допустимых значений
-        if (varZ == -1) {
+
+        while (1 + varSameDivision == 0) {
 
             System.out.println("That's an error. Please input other value of Z");
-             varZ = input.nextDouble();
-            }
-
-        // Определяем переменную для одинакового подвыражения
-        double varSameDivision = Math.cos(2*varZ-1);
+            varZ = input.nextDouble();
+            varSameDivision = Math.cos(2 * varZ - 1);
+        }
 
         // Решаем и показываем решение уравнения А
 
-        double SolutionA = (Math.log10(2+Math.exp(varX))+Math.pow((8+varY),(1/3))) / (1+varSameDivision);
+        double SolutionA = (Math.log10(2 + Math.exp(varX)) + Math.pow((8 + varY), (1 / 3))) / (1 + varSameDivision);
         System.out.println("Solution to equation A is = " + SolutionA);
 
         // Решаем и показываем решение уравнения B
 
         double SolutionB = varSameDivision + Math.exp(varX);
         System.out.println("Solution to equation B is = " + SolutionB);
-
 
 
     }
